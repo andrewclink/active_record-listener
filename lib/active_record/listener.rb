cf57a86fcce0f162cf132356a782cd597b4cc604
Andrew Clink <andrew@elementsmi.com>
2019-12-19 11:35:52 -0700

    initial commit

---
+++ lib/active_record/listener.rb
@@ -0,0 +1,8 @@
+require "active_record/listener/version"
+
+module ActiveRecord
+  module Listener
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/active_record/listener_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ActiveRecord::Listener do
+  it "has a version number" do
+    expect(ActiveRecord::Listener::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
